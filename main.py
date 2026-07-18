6211f4f0d134569aa307c2ef0b17caea25a1545a
code7wormer <shardulsingh.01@gmail.com>
2026-07-19 03:40:24 +0530

    endpoints scaffolding

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,status
 from models import Post
 app=FastAPI()
 
-@app.get("/")
+@app.get("/posts")
 def get_posts():
    return {"message":"posts"}
 
 
-@app.post("/create_post")
+@app.post("/posts")
 def create_post(new_post:Post):
    print(new_post)
    return {"data":new_post}
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+   #perform db searcg operation
+   if not row:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with ID {id} couldn't be found")
+   return {"data":"dummy single post data"}
+
+@app.put("/posts/{id}")
+def update_post(id:int):
+   #update row in db
+   return { "data":"return updated post dta/ success code"}
+
+@app.delete("/posts/{id}",status_code=204)
+def delete_post():
+   #delete row 
+   return   
--- models.py
+++ models.py
@@ -1,8 +1,7 @@
-from pydantic import BaseModel
-from pydantic import 
+from pydantic import BaseModel, Field
 
 class Post(BaseModel):
   title:str
   content:str
   published:bool = True
-  rating:str | None = None
+  rating:int | None = Field(default=None, ge=0, le=10)
